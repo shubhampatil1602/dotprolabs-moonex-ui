3e560e3c8eba0bc985567e9ccab804f33ae52bdd
Shubham Patil <shubhamspatilnbr@gmail.com>
2024-11-27 03:26:09 +0530

    fix

--- src/components/OurFeatures.jsx
+++ src/components/OurFeatures.jsx
@@ -8,7 +8,10 @@ const OurFeatures = () => {
       </h2>
       <div className='md:px-10 flex flex-wrap gap-6 justify-between items-center'>
         {features.map(({ title, des, img }) => (
-          <div className='lg:w-[296px] w-full h-[281px] border border-[#ffffff]/5 bg-[#ffffff]/[2%] hover:bg-[#ffffff]/[4%] rounded-[18px] p-[1.875rem] pl-[1.75rem] transition-all duration-300 hover:shadow-[0_0_20px_rgba(255,230,143,0.1)]'>
+          <div
+            key={img}
+            className='lg:w-[296px] w-full h-[281px] border border-[#ffffff]/5 bg-[#ffffff]/[2%] hover:bg-[#ffffff]/[4%] rounded-[18px] p-[1.875rem] pl-[1.75rem] transition-all duration-300 hover:shadow-[0_0_20px_rgba(255,230,143,0.1)]'
+          >
             <img src={img} alt={title} />
             <h4 className='text-xl leading-[1.875rem] text-[#e6f1f0] mb-3 mt-8'>
               {title}
